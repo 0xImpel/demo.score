5cc7e114797e87ec22a64e776d442532797279a5
0xImpel <111238451+0xImpel@users.noreply.github.com>
2022-08-20 14:14:10 +0200

    commit initial files (demo)

---
+++ co0dc43a1b88ec72767695f7b/index.js
@@ -0,0 +1,58 @@
+let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+let homeScoreBtnThree = document.getElementById("home-score-btn-3")
+
+let guestScoreBtnOne = document.getElementById("guest-score-btn-1")
+let guestScoreBtnTwo = document.getElementById("guest-score-btn-2")
+let guestScoreBtnThree = document.getElementById("guest-score-btn-3")
+
+let guestScoreEl = document.getElementById("guest-score")
+guestScore = 0 
+guestScoreEl.textContent = guestScore;
+
+let homeScoreEl = document.getElementById("home-score")
+homeScore = 0
+homeScoreEl.textContent = homeScore;
+
+
+
+
+
+
+
+function increaseHomeScoreOne() {
+  homeScore += 1;
+  homeScoreEl.textContent = homeScore;
+}
+
+
+function increaseHomeScoreTwo() {
+  homeScore += 2;
+  homeScoreEl.textContent = homeScore;
+}
+
+
+function increaseHomeScoreThree() {
+  homeScore += 3;
+  homeScoreEl.textContent = homeScore;
+}
+
+function increaseGuestScoreOne() {
+    guestScore += 1;
+    guestScoreEl.textContent = guestScore;
+
+}
+
+function increaseGuestScoreTwo() {
+    guestScore += 2;
+    guestScoreEl.textContent = guestScore;
+
+}
+
+function increaseGuestScoreThree() {
+    guestScore += 3;
+    guestScoreEl.textContent = guestScore;
+
+}
+
+console.log(homeScore)
